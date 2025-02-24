6aa441bf8d1fe863a55b8d25d232ac153ea23de1
luzalbaposse <malbaposse@mail.utdt.edu>
2025-02-24 22:57:07 +0100

    fix: slung

--- the-blog/app/page.tsx
+++ the-blog/app/page.tsx
@@ -94,7 +94,7 @@ export default async function Page() {
         {/* Blog Grid */}
         <div className="grid md:grid-cols-3 gap-8 mb-12">
           {posts.map((post) => (
-            <Link href={`/the-blog/${post.slug}`} key={post.slug}>
+            <Link href={`/articles/${post.slug}`} key={post.slug}>
               <article className="group cursor-pointer border-2 border-[#734F4F] p-6 rounded-lg">
                 <div className="mb-4">
                   {post.coverImage && (
